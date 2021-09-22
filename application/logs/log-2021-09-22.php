28137dd9018e2e77b6b20dc32fdb335113436dd5
arisman25 <42736874+arisman25@users.noreply.github.com>
2021-09-22 11:28:26 +0700

    new

---
+++ application/logs/log-2021-09-22.php
@@ -0,0 +1,5 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-09-22 06:24:14 --> 404 Page Not Found: /index
+ERROR - 2021-09-22 06:24:21 --> 404 Page Not Found: /index
+ERROR - 2021-09-22 06:24:37 --> 404 Page Not Found: /index
